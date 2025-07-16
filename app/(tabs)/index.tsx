a2f51e60e09f2cfbbe0792f432055250d51a3b75
TanmayAggarwal87 <tanmayagg.2005@gmail.com>
2025-07-16 15:52:01 +0530

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const {user} = useAuthStore()
-  console.log("User:",JSON.stringify(user),null,2)
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className=" flex flex-between flex-row w-full my-5 px-5">
